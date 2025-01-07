2d6488ecc6827093aa01495893d0f62317881a39
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-07 10:38:18 +0530

    added Skeleton XLMRobertaModelForMRP with dropout and classification layer

--- src/models/custom_models.py
+++ src/models/custom_models.py
@@ -69,3 +69,60 @@ class XLMRobertaCustomForTCwMRP(XLMRobertaForTokenClassification):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
+
+
+class XLMRobertaModelForMRP(XLMRobertaModel):
+    def __init__(self, config):
+        super(XLMRobertaModelForMRP, self).__init__(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.mrp_classifier = nn.Linear(config.hidden_size, 2)
+        self.init_weights()
+    
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+
+        sequence_output = self.dropout(sequence_output)
+        mrp_logits = self.mrp_classifier(sequence_output)
+
+        loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            mrp_loss = loss_fct(mrp_logits.view(-1, 2), labels.view(-1))
+            loss = mrp_loss
+
+        if not return_dict:
+            output = (mrp_logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return TokenClassifierOutput(
+            loss=loss,
+            logits=mrp_logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
