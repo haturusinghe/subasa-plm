674bae5ee19e3251fef99e1e5e5f39a21613ac50
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-07 10:26:21 +0530

    add import for custom XLMRoberta model for MRP in main.py

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ from src.config.config import ModelConfig
 from src.utils.logging_utils import setup_logging
 from src.utils.helpers import get_device, add_tokens_to_tokenizer
 
+from src.models.custom_models import XLMRobertaCustomForTCwMRP
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Subasa - Adapting Language Models for Low Resourced Offensive Language Detection in Sinhala')
 
