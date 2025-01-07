91dcf3a5944e588a35948ba2ef77e68ac846c98b
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-07 08:46:40 +0530

    add constructor to ModelConfig for initializing model parameters

--- src/config/config.py
+++ src/config/config.py
@@ -14,3 +14,11 @@ class ModelConfig:
     logging_steps: int = 100
     eval_steps: int = 500
     save_steps: int = 1000
+
+    def __init__(self, model_name, max_length, batch_size, learning_rate, epochs, output_dir):
+        self.model_name = model_name
+        self.max_length = max_length
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.output_dir = output_dir
