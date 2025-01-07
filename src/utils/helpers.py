c2f7a471ce1038ae89dcc7ad4fa00705e6992d40
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-07 10:18:56 +0530

    add function to include additional special tokens in tokenizer

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -7,3 +7,12 @@ def get_device():
     else:
         print("device = cpu")
         return torch.device('cpu')
+
+def add_tokens_to_tokenizer(args, tokenizer):
+    special_tokens_dict = {'additional_special_tokens': 
+                            ['<user>', '<number>']}  # hatexplain
+    n_added_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+    # print(tokenizer.all_special_tokens) 
+    # print(tokenizer.all_special_ids)
+    
+    return tokenizer
