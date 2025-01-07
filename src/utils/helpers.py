749a1cd0706dea3ebea13560ac0fef1681e8dc1a
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-07 08:43:57 +0530

    add helper function to determine device (CPU or CUDA)

---
+++ src/utils/helpers.py
@@ -0,0 +1,9 @@
+import torch
+
+def get_device():
+    if torch.cuda.is_available():
+        print("device = cuda")
+        return torch.device('cuda')
+    else:
+        print("device = cpu")
+        return torch.device('cpu')
