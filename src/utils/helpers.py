2d293e6971cf3d7f40a8e8be51c9dc0aab296590
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-08 14:36:42 +0530

    add NumpyEncoder for JSON serialization of numpy types

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,3 +1,5 @@
+import json
+import numpy as np
 import torch
 from sklearn.preprocessing import LabelEncoder
 import os  
@@ -161,3 +163,19 @@ def get_checkpoint_path(args):
         emb_path = path + '/' + path.split('/')[-1] + '_emb.ckpt'
         model_path_best  = path + '/' + 'BEST_' + path.split('/')[-1] + '.ckpt'
         return model_path, emb_path , model_path_best
+
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+            np.int16, np.int32, np.int64, np.uint8,
+            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32, 
+            np.float64)):
+            return float(obj)
+        elif isinstance(obj,(np.ndarray,)): #### This is the fix
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
