83df7e35af79d9325d4d7cd45fece4bbf3a3a15e
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-08 11:41:50 +0530

    add XLMRobertaForMaskedLM import

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch_optimizer as optim
 
-from transformers import XLMRobertaTokenizer, XLMRobertaModel, XLMRobertaConfig,XLMRobertaForTokenClassification, XLMRobertaForSequenceClassification
+from transformers import XLMRobertaTokenizer, XLMRobertaModel, XLMRobertaConfig,XLMRobertaForTokenClassification, XLMRobertaForSequenceClassification , XLMRobertaForTokenClassification,XLMRobertaForMaskedLM
 
 import argparse
 import logging
