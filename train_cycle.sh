7c8f221f2accc4edf41c6b13d2be527fe473b2aa
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-22 02:33:28 +0530

    update sh

--- train_cycle.sh
+++ train_cycle.sh
@@ -1,12 +1,12 @@
 # Baseline
 
-python main.py --pretrained_model xlm-roberta-base --val_int 1000 --patience 3 --epochs 5 --batch_size 16 --lr 0.00002 --seed 66 --wandb_project ablataion-study-ctrl-1 --finetuning_stage pre --dataset sold --n_tk_label 2 --skip_empty_rat True --intermediate mrp --mask_ratio 0.5 --push_to_hub True --exp_save_name mrp_0.5_seed43 --short_name True
+python main.py --pretrained_model xlm-roberta-base --val_int 1000 --patience 3 --epochs 5 --batch_size 16 --lr 0.00002 --seed 66 --wandb_project ablataion-study-ctrl-1 --finetuning_stage pre --dataset sold --n_tk_label 2 --skip_empty_rat True --intermediate mrp --mask_ratio 0.5 --push_to_hub True --exp_save_name mrp_0.5_seed66 --short_name True
 
-python main.py --pretrained_model xlm-roberta-base --val_int 2500 --patience 3 --epochs 1 --batch_size 1 --seed 66 --wandb_project ablataion-study-ctrl-1 --finetuning_stage pre --dataset sold --n_tk_label 2 --skip_empty_rat True --intermediate mrp --test True --mask_ratio 0.5 --test_model_path pre_finetune/mrp_0.5_seed43/ep4.ckpt
+python main.py --pretrained_model xlm-roberta-base --val_int 2500 --patience 3 --epochs 1 --batch_size 1 --seed 66 --wandb_project ablataion-study-ctrl-1 --finetuning_stage pre --dataset sold --n_tk_label 2 --skip_empty_rat True --intermediate mrp --test True --mask_ratio 0.5 --test_model_path pre_finetune/mrp_0.5_seed66/ep4.ckpt
 
-python main.py --pretrained_model xlm-roberta-base --val_int 1000 --patience 3 --epochs 5 --batch_size 16 --lr 0.00002 --seed 66 --wandb_project ablataion-study-ctrl-1 --finetuning_stage final --dataset sold --num_labels 2 --mask_ratio 0.5 --pre_finetuned_model pre_finetune/mrp_0.5_seed43/ep4.ckpt --exp_save_name final_w_mrp_0.5_seed43 --short_name True --push_to_hub True
+python main.py --pretrained_model xlm-roberta-base --val_int 1000 --patience 3 --epochs 5 --batch_size 16 --lr 0.00002 --seed 66 --wandb_project ablataion-study-ctrl-1 --finetuning_stage final --dataset sold --num_labels 2 --mask_ratio 0.5 --pre_finetuned_model pre_finetune/mrp_0.5_seed66/ep4.ckpt --exp_save_name final_w_mrp_0.5_seed66 --short_name True --push_to_hub True
 
-python main.py --pretrained_model xlm-roberta-base --val_int 1000 --patience 3 --epochs 1 --batch_size 1 --lr 0.00002 --seed 66 --wandb_project ablataion-study-ctrl-1 --finetuning_stage final --dataset sold --num_labels 2 --mask_ratio 0.5 --test True --test_model_path final_finetune/final_w_mrp_0.5_seed43/ep4.ckpt --push_to_hub True
+python main.py --pretrained_model xlm-roberta-base --val_int 1000 --patience 3 --epochs 1 --batch_size 1 --lr 0.00002 --seed 66 --wandb_project ablataion-study-ctrl-1 --finetuning_stage final --dataset sold --num_labels 2 --mask_ratio 0.5 --test True --test_model_path final_finetune/final_w_mrp_0.5_seed66/ep4.ckpt --push_to_hub True
 
 # Mask Ratio 0.1
 
