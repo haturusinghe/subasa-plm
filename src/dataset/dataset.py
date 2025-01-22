cc827c8324586e81459f5279bcfdbb4b7972879c
haturusinghe <s.haturusinghe99@gmail.com>
2025-01-22 10:35:06 +0530

    fix: update post_id generation to use long type for augmented dataset items

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -262,7 +262,7 @@ class SOLDAugmentedDataset(SOLDDataset):
                     augmented_sentence = ' '.join(augmented_tokens)
                 
                     new_item = {
-                        'post_id': int(f"{item['post_id']}999"),
+                        'post_id': long(f"{item['post_id']}00"),
                         'text': augmented_sentence,
                         'tokens': augmented_sentence,
                         'rationales': str(augmented_rationale),
